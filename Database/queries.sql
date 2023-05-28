5631207defef02fccd89ca2fc0625a1fc76b3d3a
Santiago Benitez <A01782813@tec.mx>
2023-05-27 22:24:39 -0600

    added queries.sql file

--- Database/queries.sql
+++ Database/queries.sql
@@ -1,13 +1,4 @@
--- The most created weapons by users
+-- The most created weapons by players
 
-
-
-SELECT weapon_id, count AS `most_created_weapon` from
-
- AS MOST_CREATED where count = MAX(most_created_weapon);
-  
-SELECT max(count) from
-(SELECT weapon_id, COUNT(weapon_id) AS `count` 
-FROM Player_Weapon AS count_table GROUP BY weapon_id) as `count`;
 
   
