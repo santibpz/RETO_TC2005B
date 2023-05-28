8afd473605836cbc4de10dfd578a3f72d3140514
Santiago Benitez <A01782813@tec.mx>
2023-05-27 23:50:28 -0600

    added one view

--- Database/queries.sql
+++ Database/queries.sql
@@ -1,4 +1,18 @@
 -- The most created weapons by players
 
+CREATE VIEW most_created_weapon AS
+(SELECT w_name from Weapon INNER JOIN 
+(SELECT weapon_id, COUNT(weapon_id) AS most_created_weapon_count
+FROM Player_Weapon
+GROUP BY weapon_id
+ORDER BY most_created_weapon_count DESC
+LIMIT 1) AS Result USING(weapon_id));
+
+
+-- El nivel en el que más pierden los jugadores
+
+SELECT lvl_name, COUNT(level_lose_count) AS max_level_lose_count 
+FROM G_Level GROUP BY level_lose_count
+
 
   
