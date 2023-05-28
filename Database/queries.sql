ddaa7bb74ee9d145ced76a1a1f370defd7b84c0c
Santiago Benitez <A01782813@tec.mx>
2023-05-27 23:59:31 -0600

    added new view

--- Database/queries.sql
+++ Database/queries.sql
@@ -12,7 +12,13 @@ LIMIT 1) AS Result USING(weapon_id));
 -- El nivel en el que más pierden los jugadores
 
 SELECT lvl_name, COUNT(level_lose_count) AS max_level_lose_count 
-FROM G_Level GROUP BY level_lose_count
+FROM G_Level GROUP BY level_lose_count;
 
+
+-- The two most upgraded weapons in the game
+SELECT w.w_name, w.w_type, w.upgrade_count 
+FROM Weapon w 
+ORDER BY w.upgrade_count DESC 
+LIMIT 2;
 
   
