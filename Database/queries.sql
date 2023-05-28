2bf38a23017de196465df4f8377b7ee6a67ac678
Santiago Benitez <A01782813@tec.mx>
2023-05-28 10:47:43 -0600

    added final views

--- Database/queries.sql
+++ Database/queries.sql
@@ -1,7 +1,7 @@
--- The most created weapons by players
+-- The most created weapon by players
 
 CREATE VIEW most_created_weapon AS
-(SELECT w_name from Weapon INNER JOIN 
+(SELECT weapon_name from Weapon INNER JOIN 
 (SELECT weapon_id, COUNT(weapon_id) AS most_created_weapon_count
 FROM Player_Weapon
 GROUP BY weapon_id
@@ -9,16 +9,32 @@ ORDER BY most_created_weapon_count DESC
 LIMIT 1) AS Result USING(weapon_id));
 
 
--- El nivel en el que más pierden los jugadores
 
-SELECT lvl_name, COUNT(level_lose_count) AS max_level_lose_count 
-FROM G_Level GROUP BY level_lose_count;
+-- The level where players lose the most (hardest level)
+
+CREATE VIEW hardest_level AS
+SELECT lvl_name
+FROM Game_Level
+ORDER BY level_lose_count DESC
+LIMIT 1;
+
+-- Which way do players lose the most? Wolf death or Player death
+
+CREATE VIEW most_frequent_death_type AS
+SELECT death_type FROM Death_Type INNER JOIN
+(SELECT death_type_id, COUNT(death_type_id) AS count
+FROM Player_Death GROUP BY death_type_id
+ORDER BY count DESC
+LIMIT 1) AS Result USING(death_type_id);
 
 
 -- The two most upgraded weapons in the game
-SELECT w.w_name, w.w_type, w.upgrade_count 
-FROM Weapon w 
-ORDER BY w.upgrade_count DESC 
+
+CREATE VIEW most_upgraded_weapons AS
+SELECT weapon_name 
+FROM Weapon
+ORDER BY upgrade_count DESC 
 LIMIT 2;
+
 
   
