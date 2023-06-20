047ebeeefc218868ac80e6fe4447b770be1b21c8
Santiago Benitez <A01782813@tec.mx>
2023-06-19 21:19:58 -0600

    updated env variables

---
+++ Web/server/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 3000
+export const HOST = process.env.HOST || "localhost"
+export const USER = process.env.USER || "videogame_user"
+export const PASSWORD = process.env.PASSWORD || "wildfrontier.tec"
+export const DATABASE = process.env.DATABASE || "WildFrontier"
+
+
