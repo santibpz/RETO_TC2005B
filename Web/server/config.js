be289e49fc4692d596f9d2846d64594bdeebd6cd
Santiago Benitez <A01782813@tec.mx>
2023-06-20 16:20:05 -0600

    added new ports

--- Web/server/config.js
+++ Web/server/config.js
@@ -1,7 +1,11 @@
+import { config } from 'dotenv'
+config()
+
 export const PORT = process.env.PORT || 3000
 export const HOST = process.env.HOST || "localhost"
 export const USER = process.env.USER || "videogame_user"
 export const PASSWORD = process.env.PASSWORD || "wildfrontier.tec"
+export const DB_PORT = process.env.DB_PASS || 3306
 export const DATABASE = process.env.DATABASE || "WildFrontier"
 
 
